854965b0f5b6c61ecc697600d4b2748fd423d68b
Tequila <46905174+fajarfendika@users.noreply.github.com>
2019-03-15 04:47:55 +0700

    Add files via upload
    bla...blaa...blaaa....

---
+++ Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+
+	'Using Metro Framework
+	Inherits MetroFramework.Forms.MetroForm
+
+	'Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+		Me.ts_Transaction = New System.Windows.Forms.ToolStripButton()
+		Me.ts_StockMaster = New System.Windows.Forms.ToolStripButton()
+		Me.ts_Customer = New System.Windows.Forms.ToolStripButton()
+		Me.ts_ManageUser = New System.Windows.Forms.ToolStripButton()
+		Me.ts_Setting = New System.Windows.Forms.ToolStripButton()
+		Me.ts_Report = New System.Windows.Forms.ToolStripButton()
+		Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+		Me.ts_Login = New System.Windows.Forms.ToolStripButton()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.ToolStrip1.SuspendLayout()
+		Me.Panel1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.BackColor = System.Drawing.Color.DeepPink
+		Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+		Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Label1.Font = New System.Drawing.Font("Hobo Std", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.White
+		Me.Label1.Location = New System.Drawing.Point(20, 60)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(802, 109)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Inventory Management System" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "CV. PASIFIC INDAH"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'ToolStrip1
+		'
+		Me.ToolStrip1.BackColor = System.Drawing.Color.Transparent
+		Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(30, 30)
+		Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ts_Transaction, Me.ts_StockMaster, Me.ts_Customer, Me.ts_ManageUser, Me.ts_Setting, Me.ts_Report, Me.ToolStripSeparator1, Me.ts_Login})
+		Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+		Me.ToolStrip1.Name = "ToolStrip1"
+		Me.ToolStrip1.Padding = New System.Windows.Forms.Padding(0)
+		Me.ToolStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No
+		Me.ToolStrip1.Size = New System.Drawing.Size(802, 52)
+		Me.ToolStrip1.Stretch = True
+		Me.ToolStrip1.TabIndex = 1
+		Me.ToolStrip1.Text = "ToolStrip1"
+		'
+		'ts_Transaction
+		'
+		Me.ts_Transaction.BackColor = System.Drawing.Color.Cyan
+		Me.ts_Transaction.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_Transaction.Image = CType(resources.GetObject("ts_Transaction.Image"), System.Drawing.Image)
+		Me.ts_Transaction.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_Transaction.Name = "ts_Transaction"
+		Me.ts_Transaction.Size = New System.Drawing.Size(98, 49)
+		Me.ts_Transaction.Text = "TRANSACTIONS"
+		Me.ts_Transaction.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'ts_StockMaster
+		'
+		Me.ts_StockMaster.BackColor = System.Drawing.Color.DeepSkyBlue
+		Me.ts_StockMaster.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_StockMaster.Image = CType(resources.GetObject("ts_StockMaster.Image"), System.Drawing.Image)
+		Me.ts_StockMaster.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_StockMaster.Name = "ts_StockMaster"
+		Me.ts_StockMaster.Size = New System.Drawing.Size(102, 49)
+		Me.ts_StockMaster.Text = "STOCK MASTERS"
+		Me.ts_StockMaster.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'ts_Customer
+		'
+		Me.ts_Customer.BackColor = System.Drawing.Color.Cyan
+		Me.ts_Customer.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_Customer.Image = CType(resources.GetObject("ts_Customer.Image"), System.Drawing.Image)
+		Me.ts_Customer.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_Customer.Name = "ts_Customer"
+		Me.ts_Customer.Size = New System.Drawing.Size(78, 49)
+		Me.ts_Customer.Text = "CUSTOMERS"
+		Me.ts_Customer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'ts_ManageUser
+		'
+		Me.ts_ManageUser.BackColor = System.Drawing.Color.DeepSkyBlue
+		Me.ts_ManageUser.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_ManageUser.Image = CType(resources.GetObject("ts_ManageUser.Image"), System.Drawing.Image)
+		Me.ts_ManageUser.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_ManageUser.Name = "ts_ManageUser"
+		Me.ts_ManageUser.Size = New System.Drawing.Size(96, 49)
+		Me.ts_ManageUser.Text = "MANAGE USERS"
+		Me.ts_ManageUser.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'ts_Setting
+		'
+		Me.ts_Setting.BackColor = System.Drawing.Color.Cyan
+		Me.ts_Setting.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_Setting.Image = CType(resources.GetObject("ts_Setting.Image"), System.Drawing.Image)
+		Me.ts_Setting.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_Setting.Name = "ts_Setting"
+		Me.ts_Setting.Size = New System.Drawing.Size(63, 49)
+		Me.ts_Setting.Text = "SETTINGS"
+		Me.ts_Setting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'ts_Report
+		'
+		Me.ts_Report.BackColor = System.Drawing.Color.DeepSkyBlue
+		Me.ts_Report.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_Report.Image = CType(resources.GetObject("ts_Report.Image"), System.Drawing.Image)
+		Me.ts_Report.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_Report.Name = "ts_Report"
+		Me.ts_Report.Size = New System.Drawing.Size(60, 49)
+		Me.ts_Report.Text = "REPORTS"
+		Me.ts_Report.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		Me.ts_Report.ToolTipText = "REPORTS"
+		'
+		'ToolStripSeparator1
+		'
+		Me.ToolStripSeparator1.BackColor = System.Drawing.SystemColors.WindowText
+		Me.ToolStripSeparator1.ForeColor = System.Drawing.Color.Black
+		Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+		Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 52)
+		'
+		'ts_Login
+		'
+		Me.ts_Login.BackColor = System.Drawing.Color.Cyan
+		Me.ts_Login.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ts_Login.Image = CType(resources.GetObject("ts_Login.Image"), System.Drawing.Image)
+		Me.ts_Login.ImageTransparentColor = System.Drawing.Color.Magenta
+		Me.ts_Login.Name = "ts_Login"
+		Me.ts_Login.Size = New System.Drawing.Size(46, 49)
+		Me.ts_Login.Text = "LOGIN"
+		Me.ts_Login.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+		'
+		'Panel1
+		'
+		Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+			Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+		Me.Panel1.BackColor = System.Drawing.Color.Transparent
+		Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+		Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Panel1.Controls.Add(Me.ToolStrip1)
+		Me.Panel1.ForeColor = System.Drawing.SystemColors.MenuText
+		Me.Panel1.Location = New System.Drawing.Point(20, 169)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(802, 416)
+		Me.Panel1.TabIndex = 2
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(842, 568)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Panel1)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Form1"
+		Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow
+		Me.Style = MetroFramework.MetroColorStyle.Magenta
+		Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+		Me.ToolStrip1.ResumeLayout(False)
+		Me.ToolStrip1.PerformLayout()
+		Me.Panel1.ResumeLayout(False)
+		Me.Panel1.PerformLayout()
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents ToolStrip1 As ToolStrip
+	Friend WithEvents ts_Transaction As ToolStripButton
+	Friend WithEvents ts_StockMaster As ToolStripButton
+	Friend WithEvents ts_Customer As ToolStripButton
+	Friend WithEvents ts_ManageUser As ToolStripButton
+	Friend WithEvents ts_Setting As ToolStripButton
+	Friend WithEvents ts_Report As ToolStripButton
+	Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+	Friend WithEvents ts_Login As ToolStripButton
+	Friend WithEvents Panel1 As Panel
+End Class
---
+++ LoginForm1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")>
+Partial Class LoginForm1
+
+	'Using Metro Framework
+	Inherits MetroFramework.Forms.MetroForm
+
+	'Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginForm1))
+		Me.lbldate = New System.Windows.Forms.Label()
+		Me.lbltime = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.txtpass = New System.Windows.Forms.TextBox()
+		Me.txtusername = New System.Windows.Forms.TextBox()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.Button1 = New System.Windows.Forms.Button()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'lbldate
+		'
+		Me.lbldate.AutoSize = True
+		Me.lbldate.Font = New System.Drawing.Font("Digital-7 Mono", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbldate.Location = New System.Drawing.Point(86, 286)
+		Me.lbldate.Name = "lbldate"
+		Me.lbldate.Size = New System.Drawing.Size(63, 20)
+		Me.lbldate.TabIndex = 20
+		Me.lbldate.Text = "Time :"
+		'
+		'lbltime
+		'
+		Me.lbltime.AutoSize = True
+		Me.lbltime.Font = New System.Drawing.Font("Digital-7 Mono", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbltime.Location = New System.Drawing.Point(86, 263)
+		Me.lbltime.Name = "lbltime"
+		Me.lbltime.Size = New System.Drawing.Size(63, 20)
+		Me.lbltime.TabIndex = 19
+		Me.lbltime.Text = "Time :"
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Font = New System.Drawing.Font("Digital-7 Mono", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.Location = New System.Drawing.Point(24, 286)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(63, 20)
+		Me.Label4.TabIndex = 17
+		Me.Label4.Text = "Date :"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Font = New System.Drawing.Font("Digital-7 Mono", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label3.Location = New System.Drawing.Point(24, 263)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(63, 20)
+		Me.Label3.TabIndex = 18
+		Me.Label3.Text = "Time :"
+		'
+		'Button3
+		'
+		Me.Button3.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.DeepPink
+		Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Pink
+		Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Pink
+		Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button3.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button3.ForeColor = System.Drawing.Color.DeepPink
+		Me.Button3.Location = New System.Drawing.Point(190, 208)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(75, 27)
+		Me.Button3.TabIndex = 16
+		Me.Button3.Text = "Close"
+		Me.Button3.UseVisualStyleBackColor = False
+		'
+		'Button2
+		'
+		Me.Button2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.DeepPink
+		Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Pink
+		Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Pink
+		Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button2.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button2.ForeColor = System.Drawing.Color.DeepPink
+		Me.Button2.Location = New System.Drawing.Point(109, 208)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(75, 27)
+		Me.Button2.TabIndex = 15
+		Me.Button2.Text = "Clear"
+		Me.Button2.UseVisualStyleBackColor = False
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.ForeColor = System.Drawing.Color.DeepPink
+		Me.Label2.Location = New System.Drawing.Point(25, 152)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(66, 16)
+		Me.Label2.TabIndex = 11
+		Me.Label2.Text = "Password :"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.DeepPink
+		Me.Label1.Location = New System.Drawing.Point(25, 98)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(67, 16)
+		Me.Label1.TabIndex = 12
+		Me.Label1.Text = "Username :"
+		'
+		'txtpass
+		'
+		Me.txtpass.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtpass.Location = New System.Drawing.Point(28, 171)
+		Me.txtpass.Name = "txtpass"
+		Me.txtpass.Size = New System.Drawing.Size(234, 22)
+		Me.txtpass.TabIndex = 13
+		Me.txtpass.UseSystemPasswordChar = True
+		'
+		'txtusername
+		'
+		Me.txtusername.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtusername.Location = New System.Drawing.Point(28, 117)
+		Me.txtusername.Name = "txtusername"
+		Me.txtusername.Size = New System.Drawing.Size(234, 22)
+		Me.txtusername.TabIndex = 10
+		'
+		'Timer1
+		'
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+		Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+		Me.PictureBox1.Location = New System.Drawing.Point(285, 101)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(188, 205)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+		Me.PictureBox1.TabIndex = 21
+		Me.PictureBox1.TabStop = False
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Font = New System.Drawing.Font("Hobo Std", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label5.Location = New System.Drawing.Point(134, 50)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(242, 34)
+		Me.Label5.TabIndex = 22
+		Me.Label5.Text = "CV. PASIFIC INDAH"
+		Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DeepPink
+		Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Pink
+		Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Pink
+		Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button1.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button1.ForeColor = System.Drawing.Color.DeepPink
+		Me.Button1.Location = New System.Drawing.Point(28, 208)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 27)
+		Me.Button1.TabIndex = 14
+		Me.Button1.Text = "&Login"
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'LoginForm1
+		'
+		Me.AcceptButton = Me.Button1
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(487, 329)
+		Me.Controls.Add(Me.Label5)
+		Me.Controls.Add(Me.lbldate)
+		Me.Controls.Add(Me.lbltime)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.txtpass)
+		Me.Controls.Add(Me.txtusername)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.ForeColor = System.Drawing.Color.DeepPink
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "LoginForm1"
+		Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow
+		Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+		Me.Style = MetroFramework.MetroColorStyle.Magenta
+		Me.Text = "Login"
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents lbldate As Label
+	Friend WithEvents lbltime As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents Button3 As Button
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label1 As Label
+	Friend WithEvents txtpass As TextBox
+	Friend WithEvents txtusername As TextBox
+	Friend WithEvents Timer1 As Timer
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents Label5 As Label
+	Friend WithEvents Button1 As Button
+End Class
